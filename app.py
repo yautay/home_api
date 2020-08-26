f0c0c374a1cb698088187c72d4da8e0f7bce0322
Michal Pielaszkiewicz <michal.pielaszkiewicz@komputronik.pl>
2020-08-26 23:21:43 +0200

    wip

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import identity, authenticate
@@ -14,7 +14,7 @@ relays = []
 
 
 class Relay(Resource):
-    @jwt_required()
+   # @jwt_required()
     def get(self, name):
         relay = next(filter(lambda x: x["relay"] == name, relays), None)
         return {"relay": relay}, 200 if relay else 404
@@ -31,9 +31,33 @@ class Relay(Resource):
         relays.append(relay)
         return relay, 201
 
-
     def put(self, name):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument("power",
+                            type=bool,
+                            required=True,
+                            help="This can not be blank!"
+                            )
+        parser.add_argument("timestamp",
+                            type=str,
+                            required=True,
+                            help="Needs time!")
+        data = parser.parse_args()
+        relay = next(filter(lambda x: x["relay"] == name, relays), None)
+        if relay is None:
+            relay = {"relay": name,
+                     "time": data["timestamp"],
+                     "power": data["power"]
+                     }
+            relays.append(relay)
+        else:
+            relay.update(data)
+        return relay
+
+    def delete(self, name):
+        global relays
+        relays = list(filter(lambda x: x["relay"] != name, relays))
+        return {"message": "Relay deleted"}
 
 
 api.add_resource(Relay, "/relay/<string:name>")
